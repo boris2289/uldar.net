d269e39b925333fd7e68c92ba56e334b9999000c
janymau <>
2026-03-05 14:57:05 +0500

    [add] added new files Model: Comments, front fixed

--- UldarFront/UldarFront/src/app/app-routing.module.ts
+++ UldarFront/UldarFront/src/app/app-routing.module.ts
@@ -20,8 +20,8 @@ import { PasswordChangeComponent } from './components/password-change/password-c
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'questions', component: QuestionsComponent },
-  { path: 'questions/:questionSlug', component: QuestionDetailComponent },
-  { path: 'questions/:questionID/edit', component: EditQuestionComponent, canActivate: [AuthGuardService], },
+  { path: 'questions/:questionSlug/', component: QuestionDetailComponent },
+  { path: 'questions/:questionSlug/edit', component: EditQuestionComponent, canActivate: [AuthGuardService], },
   { path: 'tags', component: TagsComponent },
   { path: 'tags/:tagName', component: TagDetailComponent },
   { path: 'login', component: LoginPageComponent },
